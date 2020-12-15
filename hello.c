084fe1876f68485585e8e9230033d17e55519234
Maksym <53151578+MAXSSYPE@users.noreply.github.com>
2020-12-16 00:03:54 +0200

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,91 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/printk.h>
+
+#include <linux/types.h>
+
+#include <linux/slab.h>
+
+#include <linux/ktime.h>
+
+struct personal_list_head {
+    struct personal_list_head *next;
+    ktime_t time;
+};
+
+MODULE_AUTHOR("Dudka Maksym <dudkamv@gmail.com>");
+MODULE_DESCRIPTION("Hello, world in Linux Kernel Training");
+MODULE_LICENSE("Dual BSD/GPL");
+
+static struct personal_list_head *header;
+
+static uint numberOfRepeats = 1;
+module_param(numberOfRepeats, uint, S_IRUGO
+);
+MODULE_PARM_DESC(numberOfRepeats,
+"The amount of times to print hello world");
+
+static int __init
+
+initter(void) {
+    uint i = 0;
+    struct personal_list_head *first_variable, *second_variable;
+
+    printk(KERN_INFO
+    "numberOfRepeats: %d\n", numberOfRepeats);
+
+    if (numberOfRepeats == 0) {
+        printk(KERN_WARNING
+        "The numberOfRepeats is 0");
+    } else if (numberOfRepeats >= 5 && numberOfRepeats <= 10) {
+        printk(KERN_WARNING
+        "The numberOfRepeats is between 5 and 10");
+    } 
+    
+    BUG_ON(numberOfRepeats > 10);
+
+    header = kmalloc(sizeof(struct personal_list_head *), GFP_KERNEL);
+    first_variable = header;
+
+    for (i = 0; i < numberOfRepeats; i++) {
+    if ( i == 7){ first_variable = NULL; }
+        first_variable->next = kmalloc(sizeof(struct personal_list_head), GFP_KERNEL);
+        first_variable->time = ktime_get();
+        pr_info("Hello World!");
+        second_variable = first_variable;
+        first_variable = first_variable->next;
+    }
+    if (numberOfRepeats != 0) {
+        kfree(second_variable->next);
+        second_variable->next = NULL;
+
+    }
+    printk(KERN_INFO
+    "numberOfRepeats: %d\n", numberOfRepeats);
+    return 0;
+}
+
+static void __exit
+
+exitter(void) {
+    struct personal_list_head *variable;
+    while (header != NULL && numberOfRepeats != 0) {
+        variable = header;
+        pr_info("Time : %lld", variable->time);
+        header = variable->next;
+        kfree(variable);
+    }
+    if (numberOfRepeats == 0) {
+        pr_info("Time is 0 because print wasn't used");
+    }
+    pr_info("");
+}
+
+module_init(initter);
+module_exit(exitter);
+
+
+
+
+ 
